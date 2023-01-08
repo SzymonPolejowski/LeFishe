fd67185828548c20dce6f19eee5a77f28cabd077
Szymon <szymon.polejowski@outlook.com>
2023-01-08 20:29:12 +0100

    first steps into EnlightenedStatusEffect

--- src/main/java/lefishe/main/effects/EnlightenedStatusEffect.java
+++ src/main/java/lefishe/main/effects/EnlightenedStatusEffect.java
@@ -1,5 +1,7 @@
 package lefishe.main.effects;
 
+import net.minecraft.block.LightBlock;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 
@@ -8,5 +10,10 @@ public class EnlightenedStatusEffect extends StatusEffect {
 	public EnlightenedStatusEffect() {
 		super(StatusEffectCategory.BENEFICIAL, 0xfaf58e);
 	}
+	@Override
+	public void applyUpdateEffect(LivingEntity entity, int amplifier){
+		entity.getWorld().setBlockState(entity.getBlockPos(), LightBlock.);
+	}
 	
 }
+//LightBlock.getStateFromRawId(amplifier == 1 ? 8 : 15)
