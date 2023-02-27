4502f727e7a72928d9e76b9ec9408ffcba48dcf3
SzymonP <szymon.polejowski@outlook.com>
2023-02-27 14:11:04 +0100

    Fixed enlightened bug

--- src/main/java/lefishe/main/effects/EnlightenedStatusEffect.java
+++ src/main/java/lefishe/main/effects/EnlightenedStatusEffect.java
@@ -18,7 +18,7 @@ public class EnlightenedStatusEffect extends StatusEffect {
 	@Override
 	public boolean canApplyUpdateEffect(int duration, int amplifier) {
 		//if effect is about to end, delete all light blocks
-		if (duration < 3){
+		if (duration < 3 && lightBlocks != null){
 			lightBlocks.deleteAll();
 			return false;
 		}
