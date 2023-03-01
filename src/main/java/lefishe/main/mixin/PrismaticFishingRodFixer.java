6004c2132ead09103467534a13258bcb9132cdb2
SzymonP <szymon.polejowski@outlook.com>
2023-03-01 13:36:28 +0100

    minor fixes to mixin

--- src/main/java/lefishe/main/mixin/PrismaticFishingRodFixer.java
+++ src/main/java/lefishe/main/mixin/PrismaticFishingRodFixer.java
@@ -1,16 +1,23 @@
 package lefishe.main.mixin;
 
 import lefishe.main.items.FisheItems;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FishingBobberEntity.class)
-public abstract class PrismaticFishingRodFixer {
+public abstract class PrismaticFishingRodFixer extends ProjectileEntity {
+	public PrismaticFishingRodFixer(EntityType<? extends ProjectileEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
 	@Inject(at = @At("HEAD"), method = "removeIfInvalid", cancellable = true)
 	private void fixBobber(PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
 		ItemStack itemStack = player.getMainHandStack();
