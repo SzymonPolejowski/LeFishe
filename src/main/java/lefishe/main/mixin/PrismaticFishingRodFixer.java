81f9911c1479fe129e9c5a04dc616238dc4c1591
Szymon <szymon.polejowski@outlook.com>
2023-03-05 09:56:02 +0100

    Now PrismaticFishingRodFixer works for all non-vanilla fishing rods as longs they extend FishingRodItem.

--- src/main/java/lefishe/main/mixin/PrismaticFishingRodFixer.java
+++ src/main/java/lefishe/main/mixin/PrismaticFishingRodFixer.java
@@ -1,10 +1,10 @@
 package lefishe.main.mixin;
 
-import lefishe.main.items.FisheItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.FishingRodItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,11 +18,12 @@ public abstract class PrismaticFishingRodFixer extends ProjectileEntity {
 		super(entityType, world);
 	}
 
+	//fix removeIfInvalid function working only for vanilla rods
 	@Inject(at = @At("HEAD"), method = "removeIfInvalid", cancellable = true)
 	private void fixBobber(PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
 		ItemStack itemStack = player.getMainHandStack();
 		ItemStack itemStack2 = player.getOffHandStack();
-		if (itemStack.getItem() == FisheItems.FISHE_PRISMATICROD || itemStack2.getItem() == FisheItems.FISHE_PRISMATICROD){
+		if (itemStack.getItem() instanceof FishingRodItem || itemStack2.getItem() instanceof FishingRodItem){
 			cir.setReturnValue(false);
 		}
 	}
