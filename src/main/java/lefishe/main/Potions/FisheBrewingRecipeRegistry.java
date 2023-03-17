c6a276648d57b8d9c52d2dfd72efcc9cc48431d4
SzymonP <szymon.polejowski@outlook.com>
2023-03-17 10:08:18 +0100

    reverted enlightening potion coloring changes

--- src/main/java/lefishe/main/Potions/FisheBrewingRecipeRegistry.java
+++ src/main/java/lefishe/main/Potions/FisheBrewingRecipeRegistry.java
@@ -1,10 +1,6 @@
 package lefishe.main.Potions;
 
 import lefishe.main.items.FisheItems;
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.minecraft.client.color.item.ItemColorProvider;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potions;
 import net.minecraft.recipe.BrewingRecipeRegistry;
@@ -14,13 +10,5 @@ public class FisheBrewingRecipeRegistry extends net.minecraft.recipe.BrewingReci
 		BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, FisheItems.FISHE_PHOTTOCUDAFISH, FishePotions.ENLIGHTENING);
 		BrewingRecipeRegistry.registerPotionRecipe(FishePotions.ENLIGHTENING, Items.GLOWSTONE_DUST, FishePotions.STRONG_ENLIGHTENING);
 		BrewingRecipeRegistry.registerPotionRecipe(FishePotions.ENLIGHTENING, Items.REDSTONE, FishePotions.LONG_ENLIGHTENING);
-
-		ItemColorProvider temp = new ItemColorProvider() {
-			@Override
-			public int getColor(ItemStack stack, int tintIndex) {
-				return 0x000000;
-			}
-		};
-		ColorProviderRegistry.ITEM.register(temp, FishePotions.ENLIGHTENING);
 	}
 }
